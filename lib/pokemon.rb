6f4616753366a38b9e369afb41b80f930a5d63e3
SiamKing <thesiameseelectric@gmail.com>
2016-04-19 09:29:08 -0700

    peek at choo

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  @@all = []
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?,?)",name, type)
+  end
+
+  def self.find(id, database_connection)
+    database_connection.execute("SELECT * FROM pokemon WHERE id = pokemon.id")
+  end
 end
